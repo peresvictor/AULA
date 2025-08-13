186dbed6b7c808d8bd7a6562350721a797e1debf
Victor Peres <victorperes@MacBook-Pro-de-Victor.local>
2025-08-13 15:54:49 -0300

    Meu Projeto

---
+++ aula/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aula',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3306',
+        'HOST': 'localhost',
+    }
+}
